779554f37158dd5c86e2883ac656738f698d6fe4
Axel Oehmichen <aoehmichen@users.noreply.github.com>
2017-05-25 01:12:31 +0100

    Update pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
- sudo /usr/bin/anaconda/bin/pip install test_helper
+sudo /usr/bin/anaconda/bin/pip install test_helper
 
- sudo /usr/bin/anaconda/bin/pip install python-twitter
+sudo /usr/bin/anaconda/bin/pip install python-twitter
+
+sudo /usr/bin/anaconda/bin/pip install pymongo
 
 sudo /usr/bin/anaconda/bin/pip install nltk
 
